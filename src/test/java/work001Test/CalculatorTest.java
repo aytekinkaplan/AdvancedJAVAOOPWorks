29d6ac893e2772954ad7383dcf44acadefbfdcc2
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-04-22 21:06:29 +0300

    Calculator Unit Test

--- src/test/java/work001Test/CalculatorTest.java
+++ src/test/java/work001Test/CalculatorTest.java
@@ -7,31 +7,49 @@ import org.junit.Test;
 public class CalculatorTest {
     @Test
     public void testAdd() {
+        // Given
         Calculator calculator = new Calculator();
+
+        // When
         int result = calculator.add(1, 2);
+
+        // Then
         Assert.assertEquals(3, result);
     }
 
     @Test
     public void testSubtract() {
+        // Given
         Calculator calculator = new Calculator();
+
+        // When
         int result = calculator.subtract(1, 2);
+
+        // Then
         Assert.assertEquals(-1, result);
     }
 
     @Test
     public void testMultiply() {
+        // Given
         Calculator calculator = new Calculator();
+
+        // When
         int result = calculator.multiply(1, 2);
+
+        // Then
         Assert.assertEquals(2, result); // 1 * 2 = 2
-        // 2 * 2 = 4
     }
 
     @Test
     public void testDivide() {
+        // Given
         Calculator calculator = new Calculator();
-        int result = calculator.divide(1, 2);
-        Assert.assertEquals(0, result); // 1 / 2 = 0
 
+        // When
+        int result = calculator.divide(1, 2);
+
+        // Then
+        Assert.assertEquals(0, result); // 1 / 2 = 0
     }
 }
