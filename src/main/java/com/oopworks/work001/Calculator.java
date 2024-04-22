59cbdb2a8ba0a1e8b4c681c8321abe7be1292aa9
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-04-22 21:07:16 +0300

    Calculator Unit Test

--- src/main/java/com/oopworks/work001/Calculator.java
+++ src/main/java/com/oopworks/work001/Calculator.java
@@ -1,20 +1,24 @@
 package com.oopworks.work001;
 
 public class Calculator {
+    // Adds two integers
     public int add(int a, int b) {
         return a + b;
     }
 
+    // Subtracts second integer from the first
     public int subtract(int a, int b) {
         return a - b;
     }
 
+    // Multiplies two integers
     public int multiply(int a, int b) {
         return a * b;
     }
 
+    // Divides first integer by the second
+    // Assumes the second integer is not zero
     public int divide(int a, int b) {
         return a / b;
     }
-
 }
