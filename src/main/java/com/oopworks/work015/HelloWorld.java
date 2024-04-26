bb2078f2a457bbc2abd1b1ede22cbc906c665e74
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-04-26 13:26:44 +0300

    Unit Test

---
+++ src/main/java/com/oopworks/work015/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.oopworks.work015;
+
+public class HelloWorld {
+    public static void helloWorld(String message) {
+        if (!message.isEmpty()) {
+            System.out.println(message);
+        } else
+            System.out.println("Hello World");
+
+    }
+}
---
+++ src/main/java/com/oopworks/work015/HelloWorldTry.java
@@ -0,0 +1,8 @@
+package com.oopworks.work015;
+
+public class HelloWorldTry {
+    public static void main(String[] args) {
+    HelloWorld.helloWorld("My message is here");
+    HelloWorld.helloWorld("");
+    }
+}
